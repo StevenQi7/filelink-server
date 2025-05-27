5948e7532a2a4852b4709f13c712f9aa2ce13fb5
Steven Qi <strani@163.com>
2025-05-27 10:04:40 +0800

    优化 STUN 服务器配置，移除冗余条目并更新服务器地址

--- public/js/webrtc.js
+++ public/js/webrtc.js
@@ -21,21 +21,7 @@ const RTC_CONF = {
 		{ urls: "stun:stun1.l.google.com:19302" },
 		{ urls: "stun:stun2.l.google.com:19302" },
 		{ urls: "stun:stun3.l.google.com:19302" },
-		{ urls: "stun:stun4.l.google.com:19302" },
-		// { urls: "stun:stun01.sipphone.com" },
-		// { urls: "stun:stun.ekiga.net" },
-		// { urls: "stun:stun.fwdnet.net" },
-		// { urls: "stun:stun.ideasip.com" },
-		// { urls: "stun:stun.iptel.org" },
-		// { urls: "stun:stun.rixtelecom.se" },
-		// { urls: "stun:stun.schlund.de" },
-		// { urls: "stun:stunserver.org" },
-		// { urls: "stun:stun.softjoys.com" },
-		// { urls: "stun:stun.voiparound.com" },
-		// { urls: "stun:stun.voipbuster.com" },
-		// { urls: "stun:stun.voipstunt.com" },
-		// { urls: "stun:stun.voxgratia.org" },
-		// { urls: "stun:stun.xten.com" }
+		{ urls: "stun:stun4.l.google.com:19302" }
 	]
 }
 
@@ -121,10 +107,6 @@ export const createWebSocket = () => {
 	ws.addEventListener("error", async e => {
 		clearInterval(keepAliveIntervalId)
 		console.error("WebSocket error: could not connect to server")
-		// throw "WebSocket error: could not connect to server"
-		// if(!isWsSupposedToClose) {
-		// 	setTimeout(createWebSocket, 1000)
-		// }
 	})
 
 	ws.addEventListener("close", e => {
